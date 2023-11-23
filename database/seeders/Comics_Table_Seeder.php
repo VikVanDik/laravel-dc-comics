160180cb591fb907c72b686f7ab6e403d7f9cc0f
VikVanDik <95vlc17@gmail.com>
2023-11-23 18:06:20 +0100

    first commit

---
+++ database/seeders/Comics_Table_Seeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class Comics_Table_Seeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $comics = config('comics')
+    }
+}
